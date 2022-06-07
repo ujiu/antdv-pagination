23f2bd496556f788e5eddc106fb25ea7cdfb9056
右究 <aisowe@sina.com>
2022-06-07 13:57:21 +0800

    done.

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+document.querySelector("#app").innerHTML = `
+<h4>分页器 a-pagination</h4>
+
+<h5>代码</h5>
+
+<pre>
+&lt;a-pagination
+  showSizeChanger
+  @showSizeChange="onSearch"
+  :size="form.size"
+  :default-current="1"
+  :total="dataListObj.totalRows"
+  :pageSize="dataListObj.pageSize"
+  @change="onSearch"
+  v-model="form.page"
+/&gt;
+</pre>
+
+
+<h5>注意</h5>
+<ol>
+  <li>size: string</li>
+  <li>@showSizeChange: (page = 1, size) => void</li>
+  <li>onSearch 的函参只用来传递(page, size)</li>
+</ol>
+
+<h5>a-table 中的 pagination</h5>
+
+<pre>
+&lt;a-table
+  :pagination="{
+    pageSize: form.size,
+    current: form.page,
+    defaultPageSize: 1,
+    total: form.total,
+    showSizeChanger: true,
+    showQuickJumper: true,
+    pageSizeOptions: ['10', '20', '30', '40', '50', '100', '200']
+  }"
+/&gt;
+</pre>
+`;
